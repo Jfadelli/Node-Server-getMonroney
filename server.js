772045f86748de87bfe231fb556e3a72095241e7
Jason Fadelli <jasonfadelli@Jasons-MacBook-Air.local>
2023-07-08 12:31:32 -0700

    new message to user test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
+const {fetchBearerToken} = require('./fetchBearerToken')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
